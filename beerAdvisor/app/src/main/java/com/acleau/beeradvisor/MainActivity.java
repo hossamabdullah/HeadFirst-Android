72b3666cecee8ea0c79d5c2b6570426320099272
Hossameldeen <hossamabdalh@gmail.com>
2019-08-06 11:40:39 +0200

    intial commit- chapter 1 and 2

---
+++ beerAdvisor/app/src/main/java/com/acleau/beeradvisor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.acleau.beeradvisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        String selectedItem = String.valueOf(color.getSelectedItem());
+
+        List<String> brandsList = expert.getBrands(selectedItem);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+}
