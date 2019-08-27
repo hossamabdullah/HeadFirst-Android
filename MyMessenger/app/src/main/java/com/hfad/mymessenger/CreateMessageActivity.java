9efb2385174e35c68a6305af737052f48e77ad1e
Hossameldeen <hossamabdalh@gmail.com>
2019-08-27 23:38:17 +0200

    add application of chapter 3

---
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,23 @@
+package com.hfad.mymessenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        EditText inputText = (EditText)view;
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, inputText.getText());
+        startActivity(intent);
+    }
+}
